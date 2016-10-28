8d77769c35ae8ad27dd7308a38330a1deb84a0a8
yoont4 <taylor.y1100@gmail.com>
2016-10-28 02:53:17 -0700

    created initial server test. To run, use the following command from the 'server' directory: 'python -m unittest tests.test_server' or 'python -m unittest discover' to run all tests

---
+++ server/tests/test_server.py
@@ -0,0 +1,18 @@
+import unittest
+import server
+from flask import Flask
+
+app = Flask(__name__)
+
+class MockTestCase(unittest.TestCase):
+	# Tests for 'server.py' mock methods
+
+	# This tests that the API is returning valid mock user data
+	# $NOTE-yoont4: this only checks that a response is returned
+	def test_mock_get_user(self):
+		client = app.test_client()
+		response = client.get('mock/api/get_user/1234')
+		self.assertTrue(response is not None)
+
+if __name__ == '__main__':
+	unittest.main()
