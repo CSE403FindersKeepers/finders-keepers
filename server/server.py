dc86b83b35038bf4b76d9834b8760b267b466d68
yoont4 <taylor.y1100@gmail.com>
2016-10-28 01:11:38 -0700

    updated server to include 1 incomplete API command. Currently returns 400 error

--- server/server.py
+++ server/server.py
@@ -2,17 +2,16 @@ from flask import Flask
 from flask import abort, jsonify, request
 
 app = Flask(__name__)
-@app.route("/")
-def hello():
-	return "Hello World!"
+
+# these are all the mock API end points.
 
 @app.route("/dummy/api/get_user/<int:user_id>", methods=["GET"])
-def get_user(user_id):
+def mock_get_user(user_id):
 	# just a dummy value for now
 	return make_dummy_user(user_id)
 
 @app.route("/dummy/api/new_user", methods=["POST"])
-def new_user():
+def mock_new_user():
 	# check for malformed json request
 	if not request.json:
 		abort(400)
@@ -20,7 +19,7 @@ def new_user():
 	return jsonify(request.json)
 
 @app.route("/dummy/api/update_user", methods=["PUT"])
-def update_user():
+def mock_update_user():
 	# check for malformed json request
 	if not request.json:
 		abort(400)
@@ -28,9 +27,10 @@ def update_user():
 	return jsonify(request.json)
 
 @app.route("/dummy/api/delete_user/<int:user_id>", methods=["DELETE"])
-def delete_user(user_id):
+def mock_delete_user(user_id):
 	return make_dummy_user(user_id)
 
+# mock API methods
 def make_dummy_user(user_id):
 	return jsonify(
 		user_id=user_id,
@@ -39,8 +39,15 @@ def make_dummy_user(user_id):
 		zipcode=12345,
 		email="test_email" + str(user_id) + "@email.com")
 
+# ---------------------------------------------------------------------
+# --------------------------- API -------------------------------------
+# ---------------------------------------------------------------------
 
-
+@app.route("/api/get_user/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+	# $NOTE-yoont4: this command is not implemented yet, need to discuss API behavior.
+	abort(400, "<get_user> is not accessable right now, sorry dawg")
+		
 
 if __name__ == "__main__":
 	app.run()
