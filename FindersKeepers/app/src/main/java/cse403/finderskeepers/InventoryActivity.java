b953475871e0d3379b9e0baf652c66af324a305a
Jared Guertin <jaredguertin@comcast.net>
2016-11-08 15:24:06 -0800

    Basic inventory menu UI. No functionality yet.

---
+++ FindersKeepers/app/src/main/java/cse403/finderskeepers/InventoryActivity.java
@@ -0,0 +1,18 @@
+package cse403.finderskeepers;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jared on 11/8/2016.
+ */
+
+public class InventoryActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
