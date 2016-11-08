0b1af1cad9774729bde8e106558f8f85c8bb9357
Kieran Hess <khess@cs.washington.edu>
2016-11-07 20:43:01 -0800

    Added item adding functionality

---
+++ FindersKeepers/app/src/main/java/cse403/finderskeepers/AddableItem.java
@@ -0,0 +1,47 @@
+package cse403.finderskeepers;
+
+import android.content.Context;
+import android.media.Image;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Kieran on 11/7/16.
+ * AddableItem is an ImageView which contains image tags
+ */
+public class AddableItem extends ImageView{
+    private List<String> tags;
+
+    public AddableItem(Context c, String tags){
+        super(c);
+        this.tags = parseTags(tags);
+    }
+
+    /**
+     * Returns a list of tags for the item
+     */
+    public List<String> getTags(){
+        List<String> tagCopy = new ArrayList<String>();
+        for(String item : tags){
+            tagCopy.add(item);
+        }
+        return tagCopy;
+    }
+
+    /**
+     * Takes in the string of tags and returns a list of
+     * tags
+     */
+    private List<String> parseTags(String tags){
+        Scanner scan = new Scanner(tags);
+        List<String> itemTags = new ArrayList<String>();
+        while(scan.hasNext()){
+            itemTags.add(scan.next());
+        }
+        scan.close();
+        return itemTags;
+    }
+}
