e9598402b35c250755cef9208452816e243e33e1
Jared <jaredng@cs.washington.edu>
2016-11-11 18:35:03 -0800

    Updated some stuff so that we can test locally -- Also on real API!

--- FindersKeepers/app/src/main/java/cse403/finderskeepers/data/UserInfoHolder.java
+++ FindersKeepers/app/src/main/java/cse403/finderskeepers/data/UserInfoHolder.java
@@ -16,7 +16,8 @@ import cse403.finderskeepers.UserAPIService;
  */
 
 public class UserInfoHolder {
-    public static final String SERVER_ADDRESS = "http://ec2-35-163-100-235.us-west-2.compute.amazonaws.com";
+    public static final String SERVER_ADDRESS = "http://10.0.2.2:5000"; //For local testing (note: still uses remote db)
+    /*"http://ec2-35-163-100-235.us-west-2.compute.amazonaws.com"; //For remote testing */
     private static final UserInfoHolder instance = new UserInfoHolder();
 
     private UserAPIService userapiservice;
