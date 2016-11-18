d4528d4b725acbe56fb27de7caa8f32a5c70d2a6
Kieran Hess <khess@cs.washington.edu>
2016-11-17 17:24:06 -0800

    added wishlist test

--- FindersKeepers/app/src/androidTest/java/cse403/finderskeepers/UserPageTagTest.java
+++ FindersKeepers/app/src/androidTest/java/cse403/finderskeepers/UserPageTagTest.java
@@ -14,7 +14,6 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
@@ -23,13 +22,13 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class UserPageTest {
+public class UserPageTagTest {
 
     @Rule
     public ActivityTestRule<SignInActivity> mActivityTestRule = new ActivityTestRule<>(SignInActivity.class);
 
     @Test
-    public void userPageTest() {
+    public void userPageTagTest() {
         ViewInteraction rc = onView(
                 allOf(withText("Sign in"),
                         withParent(allOf(withId(R.id.sign_in_button),
@@ -37,44 +36,17 @@ public class UserPageTest {
                         isDisplayed()));
         rc.perform(click());
 
-        ViewInteraction appCompatImageButton = onView(
-                allOf(withId(R.id.add_item),
-                        withParent(allOf(withId(R.id.item_list),
-                                withParent(withId(R.id.user_items_view))))));
-        appCompatImageButton.perform(scrollTo(), click());
-
-        ViewInteraction appCompatImageButton2 = onView(
-                allOf(withId(R.id.add_item_img),
-                        withParent(allOf(withId(R.id.content_inventory_page),
-                                withParent(withId(android.R.id.content)))),
-                        isDisplayed()));
-        appCompatImageButton2.perform(click());
-
         ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.editTags),
-                        withParent(allOf(withId(R.id.content_inventory_page),
-                                withParent(withId(android.R.id.content)))),
-                        isDisplayed()));
-        appCompatEditText.perform(replaceText("Brian"), closeSoftKeyboard());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.upload_button), withText("Add Item"),
-                        withParent(allOf(withId(R.id.content_inventory_page),
-                                withParent(withId(android.R.id.content)))),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-
-        ViewInteraction appCompatEditText2 = onView(
                 allOf(withId(R.id.edit_tags),
                         withParent(withId(R.id.content_home_page)),
                         isDisplayed()));
-        appCompatEditText2.perform(replaceText("Tag"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("Tag"), closeSoftKeyboard());
 
-        ViewInteraction appCompatButton2 = onView(
+        ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.update_tags), withText("Update Tags"),
                         withParent(withId(R.id.content_home_page)),
                         isDisplayed()));
-        appCompatButton2.perform(click());
+        appCompatButton.perform(click());
 
     }
 
