af95fd3e4e252ec893cb8800f6220c256bb76bd4
Courtney Dowell <dowelc@cs.washington.edu>
2016-11-21 16:50:05 -0800

    added trade & search tests. get_users_within_radius seems to only be returning users if they are in the EXACT zipcode given, radius is not used

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -353,6 +353,10 @@ def deny_trade():
 
 	return trade_handler.deny_trade(json)
 	
+# ONLY FOR TESTING DO NOT USE
+@app.route('/api/delete_trade/<int:trade_id>', methods=['DELETE'])
+def delete_trade(trade_id):
+	return trade_handler.delete_trade(trade_id)
 #----------------------------------------------------------------#
 
 @app.route('/api/get_users_within_radius', methods=['GET', 'POST'])
---
+++ server/tests/test_search_handler.py
@@ -0,0 +1,59 @@
+import unittest
+import json
+from server.server import app
+from flask import Flask, jsonify
+
+class TestSearchHandler(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        app.testing = True
+        self.app = app.test_client()
+
+        result = self.app.post('/api/create_user', data=json.dumps({
+            'email':'testsearchhandler1@email.com'
+        }), content_type='application/json')
+        data = json.loads(result.data)
+        self.actor = data['user_id']
+        self.app.put('/api/update_user', data=json.dumps({
+            'user_id':self.actor,
+            'zipcode': 97202 #portland
+        }), content_type='application/json')
+
+        result = self.app.post('/api/create_user', data=json.dumps({
+            'email':'testsearchhandler2@email.com'
+        }), content_type='application/json')
+        data = json.loads(result.data)
+        self.test_user_1m_away = data['user_id']
+        self.app.put('/api/update_user', data=json.dumps({
+            'user_id':self.test_user_1m_away,
+            'zipcode': 97201 #portland
+        }), content_type='application/json')
+
+        result = self.app.post('/api/create_user', data=json.dumps({
+            'email':'testsearchhandler2@email.com'
+        }), content_type='application/json')
+        data = json.loads(result.data)
+        self.test_user_100m_away = data['user_id']
+        self.app.put('/api/update_user', data=json.dumps({
+            'user_id':self.test_user_100m_away,
+            'zipcode': 98105 #seattle
+        }), content_type='application/json')
+
+    @classmethod
+    def tearDownClass(self):
+        self.app.delete('/api/delete_user/' + str(self.actor))
+        self.app.delete('/api/delete_user/' + str(self.test_user_1m_away))
+        self.app.delete('/api/delete_user/' + str(self.test_user_100m_away))
+
+    def test_get_users_within_radius(self):
+        result = self.app.get('/api/get_users_within_radius', data=json.dumps({
+            'zipcode':97202, #portland
+            'radius':30
+        }), content_type='application/json')
+        self.assertEquals(result.status, '200 OK')
+        data = json.loads(result.data)
+        self.assertTrue('users' in data)
+        self.assertEquals(len(data['users']), 2)
+        user1 = data['users'][0]['user_id']
+        user2 = data['users'][1]['user_id']
+        self.assertEquals(set([user1, user2]), set([self.actor, self.test_user_1m_away]))
