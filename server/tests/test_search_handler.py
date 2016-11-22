57c7092d33dad5f6c351415cf0990a87e0389387
Courtney Dowell <dowelc@cs.washington.edu>
2016-11-21 17:01:50 -0800

    added trade & search tests. get_users_within_radius seems to only be returning users if they are in the EXACT zipcode given, radius is not used

--- server/tests/test_search_handler.py
+++ server/tests/test_search_handler.py
@@ -26,11 +26,11 @@ class TestSearchHandler(unittest.TestCase):
         self.test_user_1m_away = data['user_id']
         self.app.put('/api/update_user', data=json.dumps({
             'user_id':self.test_user_1m_away,
-            'zipcode': 97202 #portland
+            'zipcode': 97201 #portland
         }), content_type='application/json')
 
         result = self.app.post('/api/create_user', data=json.dumps({
-            'email':'testsearchhandler2@email.com'
+            'email':'testsearchhandler3@email.com'
         }), content_type='application/json')
         data = json.loads(result.data)
         self.test_user_100m_away = data['user_id']
@@ -39,21 +39,32 @@ class TestSearchHandler(unittest.TestCase):
             'zipcode': 98105 #seattle
         }), content_type='application/json')
 
+        result = self.app.post('/api/create_user', data=json.dumps({
+            'email':'testsearchhandler4@email.com'
+        }), content_type='application/json')
+        data = json.loads(result.data)
+        self.test_user_same_zip = data['user_id']
+        self.app.put('/api/update_user', data=json.dumps({
+            'user_id':self.actor,
+            'zipcode': 97202 #portland
+        }), content_type='application/json')
+
     @classmethod
     def tearDownClass(self):
         self.app.delete('/api/delete_user/' + str(self.actor))
         self.app.delete('/api/delete_user/' + str(self.test_user_1m_away))
         self.app.delete('/api/delete_user/' + str(self.test_user_100m_away))
+        self.app.delete('/api/delete_user/' + str(self.test_user_same_zip))
 
-    def test_get_users_within_radius(self):
+    def test_get_users_within_radius_same_zip(self):
         result = self.app.get('/api/get_users_within_radius', data=json.dumps({
             'zipcode':97202, #portland
-            'radius':30
+            'radius':1
         }), content_type='application/json')
         self.assertEquals(result.status, '200 OK')
         data = json.loads(result.data)
         self.assertTrue('users' in data)
-        self.assertEquals(len(data['users']), 2)
+        self.assertEquals(len(data['users']), 2) # the test_user_1m_away might actually be in here too
         user1 = data['users'][0]['user_id']
         user2 = data['users'][1]['user_id']
-        self.assertEquals(set([user1, user2]), set([self.actor, self.test_user_1m_away]))
+        self.assertEquals(set([user1, user2]), set([self.actor, self.test_user_same_zip]))
